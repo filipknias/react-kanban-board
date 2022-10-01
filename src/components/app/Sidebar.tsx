7e6be0e0e71a62ecf1f7bea5089a488ab43e0d58
Filip <filip.knias@o2.pl>
2022-10-01 17:28:36 +0200

    sidebar account related buttons added

--- src/components/app/Sidebar.tsx
+++ src/components/app/Sidebar.tsx
@@ -1,5 +1,8 @@
 import Logo from '../../components/utilities/Logo';
 import BoardsList from './BoardsList';
+import { FaUserAlt, FaSignOutAlt } from 'react-icons/fa';
+import { signOut } from 'firebase/auth';
+import { auth } from '../../lib/firebase';
 
 const mockBoards = [
   { id: '1', name: 'Platform Launch' },
@@ -15,6 +18,16 @@ export default function Sidebar() {
       </div>
       <div className="py-8 flex flex-col justify-between flex-1">
         <BoardsList boards={mockBoards} />
+        <div className="px-8 flex flex-col gap-3">
+          <button className="sidebar-btn">
+            <FaUserAlt />
+            My profile
+          </button>
+          <button className="sidebar-btn" onClick={() => signOut(auth)}>
+            <FaSignOutAlt />
+            Logout
+          </button>
+        </div>
       </div>
     </div>
   )
