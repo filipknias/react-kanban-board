b7864dc12923229240cec61c91021edf1c56b900
Filip <filip.knias@o2.pl>
2022-10-03 16:57:47 +0200

    change useAsync hook to return success state

--- src/hooks/useAsync.tsx
+++ src/hooks/useAsync.tsx
@@ -3,20 +3,20 @@ import { useState } from "react";
 export default function useAsync(asyncCallback: () => Promise<void>) {
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string|null>(null);
-  const [data, setData] = useState<unknown|null>(null);
+  const [success, setSuccess] = useState<boolean>(false);
   
   const trigger = () => {
     // Reset state
     setError(null);
-    setData(null);
+    setSuccess(false);
     setLoading(true);
-    // Return data
+    // Perform async function
     const result = asyncCallback();
     result
-      .then((data) => setData(data))
+      .then(() => setSuccess(true))
       .catch((err) => setError(err.code))
       .finally(() => setLoading(false));
   };
 
-  return { loading, error, data, trigger };
+  return { loading, error, success, trigger };
 };
