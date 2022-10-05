97e89f40cfa236b58b88ca27c391d378ea855f74
Filip <filip.knias@o2.pl>
2022-10-05 21:50:50 +0200

    hide sidebar under lg breakpoint

--- src/components/app/Sidebar.tsx
+++ src/components/app/Sidebar.tsx
@@ -9,7 +9,7 @@ export default function Sidebar() {
   const { boards } = useAppSelector((state) => state.dashboard);
 
   return (
-    <div className="bg-gray-800 border-r-2 border-r-gray-700 h-full w-1/5 flex flex-col">
+    <div className="hidden lg:flex flex-col bg-gray-800 border-r-2 border-r-gray-700 h-full w-1/5">
       <div className="p-8 flex justify-center border-b-2 border-b-gray-700">
         <Logo />
       </div>
