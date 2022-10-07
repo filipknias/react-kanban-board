0d529b9958f175d94c26daa07666ef057fd5da30
Filip <filip.knias@o2.pl>
2022-10-07 15:15:55 +0200

    task modal component created without subtasks list

--- src/components/app/Task.tsx
+++ src/components/app/Task.tsx
@@ -1,17 +1,25 @@
 import { Task as TaskType } from "../../utilities/types";
 import { useMemo } from 'react';
+import { useAppDispatch } from '../../redux/hooks';
+import { openModal } from '../../redux/features/modalsSlice';
+import TaskModal from '../modals/TaskModal';
 
 interface Props {
   task: TaskType;
 }
 
 export default function Task({ task }: Props) {
+  const dispatch = useAppDispatch();
+
   const doneTasks = useMemo(() => {
     return task.subtasks.filter((subtask) => subtask.done);
   }, [task]);
 
   return (
-    <div className="bg-gray-700 rounded-md p-3 cursor-pointer transition-colors hover:bg-gray-600 flex flex-col gap-1">
+    <div 
+      className="bg-gray-700 rounded-md p-3 cursor-pointer transition-colors hover:bg-gray-600 flex flex-col gap-1"
+      onClick={() => dispatch(openModal(<TaskModal task={task} />))}  
+    >
       <h1 className="font-medium text-lg">{task.name}</h1>
       <p className="font-medium text-sm text-gray-400">{doneTasks.length} of {task.subtasks.length} subtasks</p>
     </div>
---
+++ src/components/modals/TaskModal.tsx
@@ -0,0 +1,44 @@
+import Modal from "./Modal";
+import { Task } from "../../utilities/types";
+import useSelectedBoard from '../../hooks/useSelectedBoard';
+import { useState, useEffect } from 'react';
+
+interface Props {
+  task: Task;
+}
+
+export default function TaskModal({ task }: Props) {
+  const [columnId, setColumnId] = useState<string>(task.columnId);
+  const { columns } = useSelectedBoard();
+
+  useEffect(() => {
+    updateColumnId(columnId);
+  }, [columnId]);
+
+  const updateColumnId = async (id: string) => {
+    
+  };
+
+  return (
+    <Modal>
+      <div className="modal-container flex flex-col gap-5">
+        <h1 className="text-lg font-medium">{task.name}</h1>
+        <p className="text-sm text-gray-400">{task.description ? task.description : "No description"}</p>
+        {/* SUBTASKS LIST COMPONENT */}
+        <div className="flex flex-col gap-2">
+          <label htmlFor="column-id" className="font-medium">Current Status</label>
+          <select 
+            id="column-id" 
+            className="select-input"
+            onChange={(e) => setColumnId(e.target.value)}
+            value={columnId}
+          >
+            {columns.map((column) => (
+              <option value={column.id} key={column.id}>{column.name}</option>
+            ))}
+          </select>
+        </div>
+      </div>
+    </Modal>
+  )
+}
