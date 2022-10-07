7938fadeb936219e260a22bdbc55fb41dcf999dd
Filip <filip.knias@o2.pl>
2022-10-07 14:40:22 +0200

    display number of done tasks in task component

--- src/components/app/Task.tsx
+++ src/components/app/Task.tsx
@@ -1,14 +1,19 @@
-import { Task as TaskType } from "../../utilities/types"
+import { Task as TaskType } from "../../utilities/types";
+import { useMemo } from 'react';
 
 interface Props {
   task: TaskType;
 }
 
 export default function Task({ task }: Props) {
+  const doneTasks = useMemo(() => {
+    return task.subtasks.filter((subtask) => subtask.done);
+  }, [task]);
+
   return (
     <div className="bg-gray-700 rounded-md p-3 cursor-pointer transition-colors hover:bg-gray-600 flex flex-col gap-1">
       <h1 className="font-medium text-lg">{task.name}</h1>
-      <p className="font-medium text-sm text-gray-400">0 of 1 subtasks</p>
+      <p className="font-medium text-sm text-gray-400">{doneTasks.length} of {task.subtasks.length} subtasks</p>
     </div>
   )
 };
