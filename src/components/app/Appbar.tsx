1858f13fd1549329c292297c376df947e67f1e5e
Filip <filip.knias@o2.pl>
2022-10-12 15:20:15 +0200

    display menu icon properly in appbar component

--- src/components/app/Appbar.tsx
+++ src/components/app/Appbar.tsx
@@ -11,31 +11,29 @@ export default function Appbar() {
 
   return (
     <div className="bg-gray-800 border-b-2 border-b-gray-700 py-8 px-5 h-24 flex items-center justify-between w-screen lg:w-auto">
+      <div className="flex items-center gap-3">
+        <div className="bg-purple-500 rounded-sm flex items-center justify-center p-3 cursor-pointer transition-colors hover:bg-purple-600 lg:hidden">
+          <FaBars />
+        </div>
+        {board && ( <h1 className="font-medium text-xl lg:text-2xl">{board.name}</h1>)}
+      </div>
       {board && (
-        <>
-          <div className="flex items-center gap-3">
-            <div className="bg-purple-500 rounded-sm flex items-center justify-center p-3 cursor-pointer transition-colors hover:bg-purple-600 lg:hidden">
-              <FaBars />
-            </div>
-            <h1 className="font-medium text-xl lg:text-2xl">{board.name}</h1>
-          </div>
-          <div className="flex gap-5">
-            <button 
-              className="bg-purple-600 rounded-sm p-3 text-white flex items-center gap-2 transition-colors hover:bg-purple-700 font-medium"
-              onClick={() => dispatch(openModal(<AddTaskModal />))}
-            >
-              <FaPlus />
-              <span className="hidden lg:block">Add Task</span>
-            </button>
-            <button 
-              className="bg-purple-600 rounded-sm p-3 text-white flex items-center gap-2 transition-colors hover:bg-purple-700 font-medium"
-              onClick={() => dispatch(openModal(<EditBoardModal board={board} />))}  
-            >
-              <FaCog />
-              <span className="hidden lg:block">Options</span>
-            </button>
-          </div>
-        </>    
+        <div className="flex gap-5">
+          <button 
+            className="bg-purple-600 rounded-sm p-3 text-white flex items-center gap-2 transition-colors hover:bg-purple-700 font-medium"
+            onClick={() => dispatch(openModal(<AddTaskModal />))}
+          >
+            <FaPlus />
+            <span className="hidden lg:block">Add Task</span>
+          </button>
+          <button 
+            className="bg-purple-600 rounded-sm p-3 text-white flex items-center gap-2 transition-colors hover:bg-purple-700 font-medium"
+            onClick={() => dispatch(openModal(<EditBoardModal board={board} />))}  
+          >
+            <FaCog />
+            <span className="hidden lg:block">Options</span>
+          </button>
+        </div>  
       )}
     </div>
   )
