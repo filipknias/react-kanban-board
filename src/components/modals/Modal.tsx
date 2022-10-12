f9032300146adcf173be4c4aee3950f6ebd0a016
Filip <filip.knias@o2.pl>
2022-10-12 21:24:53 +0200

    change z-index modal component class

--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -14,7 +14,7 @@ export default function Modal({ children }: Props) {
   useClickOutiside(modalRef, () => dispatch(hideModal()));
 
   return (
-    <div className="fixed top-0 left-0 h-screen w-screen bg-black bg-opacity-50 flex items-center justify-center p-5">
+    <div className="fixed top-0 left-0 h-screen w-screen bg-black bg-opacity-50 flex items-center justify-center p-5 z-10">
       <div ref={modalRef} className="bg-gray-800 rounded-sm p-5 z-10 w-full sm:w-3/4 md:w-1/2 xl:w-1/4">{children}</div>
     </div>
   )
