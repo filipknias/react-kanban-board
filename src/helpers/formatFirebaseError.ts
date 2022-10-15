9608bd7c7ed056cde64b2f2001d2360529d61a83
Filip <filip.knias@o2.pl>
2022-10-15 11:53:32 +0200

    edit format firebase error file

--- src/helpers/formatFirebaseError.ts
+++ src/helpers/formatFirebaseError.ts
@@ -5,6 +5,7 @@ export const formatFirebaseError = (message: string) => {
     case 'auth/invalid-email': return "Email should be valid address";
     case 'auth/wrong-password': return "Wrong email or password";
     case 'auth/user-not-found': return "Wrong email or password";
+    case 'auth/requires-recent-login': return "Reauthenticate with password";
     default: return "Something went wrong";
   }
 };
