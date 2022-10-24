968f821afc5adac21dc8484bea8da81998bc9eb6
Filip <filip.knias@o2.pl>
2022-10-24 18:15:45 +0200

    move profile form button to seperate component

---
+++ src/components/forms/ProfileFormButtons.tsx
@@ -0,0 +1,55 @@
+import { useEffect } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import routes from '../../utilities/routes';
+import { FaTrash, FaHome } from 'react-icons/fa'; 
+import { auth, db } from '../../lib/firebase';
+import { deleteUser } from 'firebase/auth';
+import { deleteDoc, collection, query, where, getDocs } from 'firebase/firestore';
+import useAsync from '../../hooks/useAsync';
+
+export default function ProfileFormButtons() {
+  const navigate = useNavigate();
+
+  const { loading, success, trigger } = useAsync(async () => {
+    if (auth.currentUser === null) return;
+    // Delete board docs
+    const boardsQ = query(collection(db, "boards"), where("userId", "==", auth.currentUser.uid));
+    const boardsQuerySnapshot = await getDocs(boardsQ);
+    boardsQuerySnapshot.forEach(async (doc) => deleteDoc(doc.ref));
+    // Delete columns docs
+    const columnsQ = query(collection(db, "columns"), where("userId", "==", auth.currentUser.uid));
+    const columnsQuerySnapshot = await getDocs(columnsQ);
+    columnsQuerySnapshot.forEach(async (doc) => deleteDoc(doc.ref));
+    // Delete tasks docs
+    const tasksQ = query(collection(db, "tasks"), where("userId", "==", auth.currentUser.uid));
+    const tasksQuerySnapshot = await getDocs(tasksQ);
+    tasksQuerySnapshot.forEach(async (doc) => deleteDoc(doc.ref));
+    // Delete user profile
+    await deleteUser(auth.currentUser);
+  });
+
+  useEffect(() => {
+    // Redirect to sign in page after deleting profile
+    if (success) {
+      navigate(routes.signIn);  
+    }
+  }, [success]);
+
+  return (
+    <div className="flex flex-col gap-3">
+      <Link to={routes.index}>
+        <button  className="w-full bg-blue-600 hover:bg-blue-700 transition-colors px-5 py-2 rounded-md font-medium flex items-center justify-center gap-2">
+          <FaHome />
+          Back to dashboard
+        </button>
+      </Link>
+      <button
+        className={`w-full bg-red-600 hover:bg-red-700 transition-colors px-5 py-2 rounded-md font-medium flex items-center justify-center gap-2 ${loading ? "btn-loading" : " "}`}
+        onClick={trigger}
+      >
+        <FaTrash />
+        Delete profile
+      </button>
+    </div>
+  )
+}
--- src/components/forms/UpdateProfileForm.tsx
+++ src/components/forms/UpdateProfileForm.tsx
@@ -1,43 +1,13 @@
 import { useEffect } from 'react';
-import useAsync from '../../hooks/useAsync';
-import { auth, db } from '../../lib/firebase';
-import { deleteUser } from 'firebase/auth';
-import { deleteDoc, collection, query, where, getDocs } from 'firebase/firestore';
 import { useAppDispatch } from '../../redux/hooks';
-import { FaTrash, FaHome } from 'react-icons/fa'; 
-import { useNavigate, Link } from 'react-router-dom';
-import routes from '../../utilities/routes';
 import { openModal } from '../../redux/features/modalsSlice';
 import ReauthenticateModal from '../modals/ReauthenticateModal';
 import UpdateEmailForm from './UpdateEmailForm';
 import UpdatePasswordForm from './UpdatePasswordForm';
+import ProfileFormButtons from './ProfileFormButtons';
 
 export default function UpdateProfileForm() {
-  const navigate = useNavigate();
   const dispatch = useAppDispatch();
-
-  const deleteProfileAction = useAsync(async () => {
-    if (auth.currentUser === null) return;
-    // Delete board docs
-    const boardsQ = query(collection(db, "boards"), where("userId", "==", auth.currentUser.uid));
-    const boardsQuerySnapshot = await getDocs(boardsQ);
-    boardsQuerySnapshot.forEach(async (doc) => deleteDoc(doc.ref));
-    // Delete columns docs
-    const columnsQ = query(collection(db, "columns"), where("userId", "==", auth.currentUser.uid));
-    const columnsQuerySnapshot = await getDocs(columnsQ);
-    columnsQuerySnapshot.forEach(async (doc) => deleteDoc(doc.ref));
-    // Delete tasks docs
-    const tasksQ = query(collection(db, "tasks"), where("userId", "==", auth.currentUser.uid));
-    const tasksQuerySnapshot = await getDocs(tasksQ);
-    tasksQuerySnapshot.forEach(async (doc) => deleteDoc(doc.ref));
-    // Delete user profile
-    await deleteUser(auth.currentUser);
-  });
-
-  useEffect(() => {
-    // Redirect to sign in page after deleting profile
-    if (deleteProfileAction.success) navigate(routes.signIn);
-  }, [deleteProfileAction.success]);
 
   useEffect(() => {
     // Reauthenticate user
@@ -49,21 +19,7 @@ export default function UpdateProfileForm() {
       <div className="flex flex-col gap-12">
         <UpdateEmailForm />
         <UpdatePasswordForm />
-        <div className="flex flex-col gap-3">
-          <Link to={routes.index}>
-            <button  className="w-full bg-blue-600 hover:bg-blue-700 transition-colors px-5 py-2 rounded-md font-medium flex items-center justify-center gap-2">
-              <FaHome />
-              Back to dashboard
-            </button>
-          </Link>
-          <button
-            className={`w-full bg-red-600 hover:bg-red-700 transition-colors px-5 py-2 rounded-md font-medium flex items-center justify-center gap-2 ${deleteProfileAction.loading ? "btn-loading" : " "}`}
-            onClick={() => deleteProfileAction.trigger()}
-          >
-            <FaTrash />
-            Delete profile
-          </button>
-        </div>
+        <ProfileFormButtons />
       </div>
     </div>
   )
